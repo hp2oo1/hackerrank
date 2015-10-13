82f36178156f45218702febcebf14548652244bf
hp2oo1 <huangpeng723@gmail.com>
2015-10-13 17:12:20 +0100

    Update solution.cpp

--- remove-duplicates-unsorted-linked-list/solution.cpp
+++ remove-duplicates-unsorted-linked-list/solution.cpp
@@ -1,37 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unordered_set>
-
+#include <memory>
+#include <iostream>
 
 using namespace std;
 
 /* A linked list node */
-struct node {
+struct Node {
     int data;
-    struct node *next;
+    Node *next;
+    Node() : data(0), next(0) {};
+    Node(Node&& other) noexcept : data(move(other.data)), next(move(other.next)) {}
+    ~Node() { cout<<"~Node()"<<endl; }
 };
 
 /* Function to push a node */
-void push( struct node** head_ref, int new_data ) {
-    node* new_node = new node;
+void push( Node** head_ref, int new_data ) {
+    Node* new_node = new Node;
     new_node->data  = new_data;
     new_node->next = (*head_ref);
     (*head_ref)    = new_node;
 }
 
 /* Function to print nodes in a given linked list */
-void printList( struct node *node ) {
+void printList( Node *node ) {
     while(node != NULL) {
         printf("%d->", node->data);
         node = node->next;
     }
-    printf("NULL");
+    printf("NULL\n");
 }
 
-void removeDuplicates( struct node* start ) {
+void removeDuplicates( Node* start ) {
     unordered_set<int> myset;
-    struct node* node = start;
-    struct node* node_previous = NULL;
+    Node* node = start;
+    Node* node_previous = NULL;
     while( node != NULL ) {
         if( myset.count( node->data ) == 0 ) {
             myset.insert( node->data );
@@ -40,8 +44,10 @@ void removeDuplicates( struct node* start ) {
         }
         else {
             node_previous->next = node->next;
-            struct node node_to_delete;
-            swap( node_to_delete, *node );
+            //delete node;
+            // use smart pointer to delete node
+            // avoid using delete manually
+            unique_ptr<Node> to_delete(node);
             node = node_previous->next;
         }
     }
@@ -49,7 +55,7 @@ void removeDuplicates( struct node* start ) {
 
 int main() {
     
-    struct node *start = NULL;
+    Node *start = NULL;
     
     /* The constructed linked list is:
     10->12->11->11->12->11->10*/
